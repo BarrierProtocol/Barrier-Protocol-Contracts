e125905bd8d94341853b6adec846c37b6ab8c349
BarrierProtocol <71371905+BarrierProtocol@users.noreply.github.com>
2020-09-17 17:53:06 -0700

    Rebased file to Barrier Protocol

--- contracts/BarrierToken.sol
+++ contracts/BarrierToken.sol
@@ -1,6 +1,9 @@
 pragma solidity 0.6.12;
 
-
+import "@openzeppelin/contracts/GSN/Context.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
